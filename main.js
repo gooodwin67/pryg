8155e2151ef81bf7f086028d08819cda053e94a3
Light Course <sn67@inbox.ru>
2025-07-26 16:22:52 +0300

    dep

--- main.js
+++ main.js
@@ -397,6 +397,14 @@ function onTapDown(event) {
 
 }
 function onTapUp(event) {
+  let rect = renderer.domElement.getBoundingClientRect();
+
+  event = event.changedTouches[0];
+
+  mouse.x = ((event.clientX - rect.left) / rect.width) * 2 - 1;
+  mouse.y = - ((event.clientY - rect.top) / rect.height) * 2 + 1;
+
+  raycaster.setFromCamera(mouse, camera);
   if (mouse.x > 0) {
     upKeys(players[0].player);
   }
